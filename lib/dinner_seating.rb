faa21a8fb745c6ccae40c7ad88a2890c2a3d9bdf
Ash Moran <ash.moran@patchspace.co.uk>
2015-02-07 17:53:08 +0100

    More stubs for structure

--- lib/dinner_seating.rb
+++ lib/dinner_seating.rb
@@ -1,9 +1,13 @@
 class PartyHost
   def initialize(relationship_scores)
-
+    @relationship_scores = relationship_scores
   end
 
   def seating_arrangement
-    [1]
+    (0...@relationship_scores.length).to_a
+  end
+
+  def seating_arrangement_score
+    0
   end
 end
--- spec/lib/dinner_seating_spec.rb
+++ spec/lib/dinner_seating_spec.rb
@@ -5,20 +5,41 @@ require_relative '../../lib/dinner_seating'
 # scratch without having to look up a known algorithm
 
 describe PartyHost do
+  subject(:host) { PartyHost.new(relationships) }
+
+  let(:chosen_arrangement) { host.seating_arrangement }
+  let(:chosen_arrangement_score) { host.seating_arrangement_score }
+
   context "one seat" do
-    let(:host) {
-      PartyHost.new(
-        [
-          [0]
-        ]
-      )
+    let(:relationships) {
+      [
+        [0]
+      ]
     }
 
     let(:chosen_arrangement) { host.seating_arrangement }
 
     it "includes all guests" do
-      pending "stub"
       expect(chosen_arrangement).to be == [0]
+    end
+  end
+
+  context "two seats" do
+    context "indifferent guests" do
+      let(:relationships) {
+        [
+          [0, 0],
+          [0, 0]
+        ]
+      }
+
+      it "includes all guests" do
+        expect(chosen_arrangement).to match_array([0, 1])
+      end
+
+      it "has a score 0" do
+        expect(chosen_arrangement_score).to be == 0
+      end
     end
   end
 end
